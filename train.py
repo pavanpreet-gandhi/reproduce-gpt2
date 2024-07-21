f8f063f2a0ce65080074da559b204637bd2d6b1f
Pavanpreet Singh Gandhi <pavanpreet.gandhi@gmail.com>
2024-07-21 13:10:55 +0100

    Add logging

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import math
 import time
+import logging
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
@@ -10,33 +11,44 @@ from gpt2 import GPT, GPTConfig
 from dataloader import DataLoader
 
 
+# Configure logging
+log_dir = 'logs'
+logging.basicConfig(
+    filename=f'{log_dir}/train.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+
 # Device and manual seed
 torch.manual_seed(13)
 torch.cuda.manual_seed(13)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print('Using device:', device.type)
+logging.info(f'Using {device.type} device for training')
 
 
 # Load and split dataset
-print('Loading dataset...')
 dataset = load_dataset(
     path='HuggingFaceFW/fineweb-edu', 
     name='default', # the full most recent version of the dataset with over 1.3 trillion GPT-2 tokens
     split='train', # there is only this one split
     streaming=True
 )
+logging.info(f'Dataset loaded with {dataset.dataset_size} text samples')
 dataset = dataset.shuffle(seed=13)
 train_dataset = dataset.skip(1000)
 val_dataset = dataset.take(1000)
 
 
 # Set learning rate schedule
-print('Configuring learning rate schedule...')
 max_lr = 6e-4
 min_lr = max_lr / 10
 warmup_steps = 715
-decay_steps = int(10e9 / 2**19) - warmup_steps
+decay_steps = int(10e9 / 2**19) - warmup_steps # roughly over 1 billion tokens
 num_steps = int(10e9 / 2**19) * 2 # roughly over 2 billion tokens
+assert num_steps > (warmup_steps + decay_steps), 'Total number of steps must be greater than warmup steps + decay steps'
+logging.info(f'Learning rate schedule: max_lr={max_lr}, min_lr={min_lr}, warmup_steps={warmup_steps}, decay_steps={decay_steps}, num_steps={num_steps}')
 
 def lr_scheduler(step: int) -> float:
     if step < warmup_steps:
@@ -46,38 +58,37 @@ def lr_scheduler(step: int) -> float:
     else:
         return min_lr
 
-filepath = 'logs/lr_schedule.png'
+filepath = f'{log_dir}/lr_schedule.png'
 plt.figure(figsize=(10, 6))
 plt.plot([lr_scheduler(step) for step in range(0, decay_steps + 1)])
 plt.title('Learning rate schedule')
 plt.xlabel('Step')
 plt.ylabel('Learning rate')
 plt.savefig(filepath)
-print(f'Learning rate schedule saved to {filepath}')
+logging.info(f'Learning rate schedule graph saved to {filepath}')
 
 
 # Set batch sizes
-print('Configuring batch sizes...')
 B, T = 4, 1024
 total_batch_size = 2**19
 assert total_batch_size % (B * T) == 0, 'Total batch size must be divisible by B * T'
 grad_accum_steps = total_batch_size // (B * T)
+logging.info(f'Batch sizes: B={B}, T={T}, grad_accum_steps={grad_accum_steps}')
 
 
 # Create data loaders
-print('Creating data loaders...')
 train_data_loader = DataLoader(B=B, T=T, dataset=train_dataset)
 val_data_loader = DataLoader(B=B, T=T, dataset=val_dataset)
+logging.info('Data loaders created')
 
 
 # Create model
-print('Creating model...')
 config = GPTConfig(vocab_size=50304)
 model = GPT(config).to(device)
+logging.info(f'Model created with config: {config}')
 
 
 # Configure optimizer
-print('Configuring optimizer...')
 param_dict = {pn: p for pn, p in model.named_parameters() if p.requires_grad}
 weight_decay_params = [p for pn, p in param_dict.items() if p.dim() >= 2]
 non_weight_decay_params = [p for pn, p in param_dict.items() if p.dim() < 2]
@@ -86,17 +97,17 @@ param_groups = [
     {'params': non_weight_decay_params, 'weight_decay': 0.0}
 ]
 optimizer = torch.optim.AdamW(param_groups, lr=1.0, betas=(0.9, 0.95), eps=1e-8, fused=True)
-
+logging.info(f'Optimizer created with AdamW algorithm')
 
 # Computational optimizations
-print('Applying computational optimizations...')
+logging.info('Compiling model...')
 model = torch.compile(model)
 torch.set_float32_matmul_precision('high') # requires compute capability >= 8.0
 
 
 # Training loop
-print('Starting training loop...\n')
-for step in range(decay_steps):
+logging.info('Starting training loop...\n')
+for step in range(num_steps):
     t0 = time.time()
 
     loss_accum = torch.tensor(0.0, device=device, requires_grad=False)
@@ -126,4 +137,4 @@ for step in range(decay_steps):
     t1 = time.time()
     dt = (t1 - t0) * 1000 # in milliseconds
     throughput = (B * T * grad_accum_steps) / (dt / 1000) # tokens per second
-    print(f"step {step:4d} | loss {loss_accum:.6f} | lr: {lr:.4e} | norm: {norm:.4f} | dt: {dt:.2f} ms | throughput: {throughput:.2f} tps")
+    logging.info(f"step {step:4d} | loss {loss_accum:.6f} | lr: {lr:.4e} | norm: {norm:.4f} | dt: {dt:.2f} ms | throughput: {throughput:.2f} tps")
